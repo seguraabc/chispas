5c341f4ac44ce3aa965b9cc5fc93f3dab9c14dde
Sebastian Segura <seguraabc@gmail.com>
2023-10-31 15:11:48 -0300

    primera subida

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const video = document.querySelector(".animated-video");
+    video.style.transform = "scale(1)";
+    video.style.opacity = 1;
+});
